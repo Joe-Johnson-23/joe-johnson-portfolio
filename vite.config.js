3007ffe7a0f40f7350a0c522dad9da53422a5312
Joseph Johnson <josephjohnson@Josephs-MacBook-Pro.local>
2025-12-14 19:53:42 -0600

    Update base path and homepage to joe-johnson-portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/chaosArt/',
+  base: '/joe-johnson-portfolio/',
   plugins: [react()],
   preview: {
     host: true,
